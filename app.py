1c3ab0c0b9b877620e020c120a1c9216d51537c7
rhoyyyalvinnn <94331000+rhoyyyalvinnn@users.noreply.github.com>
2025-05-07 00:11:54 +0800

    minor changes

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ with open('calorie_prediction_model.pkl', 'rb') as f:
 app = Flask(__name__)
 app.secret_key = os.getenv('FLASK_SECRET_KEY', os.urandom(24))
 
-food_items = list(encoder.categories_[0])  # For dropdown
+food_items = list(encoder.categories_[0])
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -30,23 +30,19 @@ def index():
             selected_food = request.form["food"].strip().lower()
             grams = float(request.form["grams"])
 
-            # Add the food and grams to the list in session
-            session['food_entries'].append((selected_food, grams))
-            session.modified = True  # Mark the session as modified so it gets saved
 
-            # Redirect to avoid resubmission
+            session['food_entries'].append((selected_food, grams))
+            session.modified = True 
+
             return redirect(url_for('index'))
 
         elif action == "remove":
             remove_index = int(request.form["remove_index"])
-            # Remove the selected food from the list
             session['food_entries'].pop(remove_index)
-            session.modified = True  # Mark the session as modified
+            session.modified = True
 
-            # Redirect to avoid resubmission
             return redirect(url_for('index'))
 
-    # Calculate total calories after form submission or on initial load
     total_calories = 0
     for food_name, grams in session['food_entries']:
         encoded = encoder.transform([[food_name]])
